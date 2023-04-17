8e5755831bb8fbbed1bf312447fd110c7d934b9a
Diversification <startsevprm@gmail.com>
2023-04-17 22:43:08 +0500

    add new string

--- task1.py
+++ task1.py
@@ -6,9 +6,14 @@
 __________________________________________________
 
 
-add string 9
-
-
-string 12
-
-________add string 13
+number = int(input("Введите число: "))
+if number % 2 == 0 and number < 0:
+    print("отрицательное четное число")
+elif number == 0:
+    print("ноль")
+elif number % 2 != 0 and number < 0:
+    print("отрицательное нечетное число")
+elif number % 2 == 0 and number > 0:
+    print("положительное четное число")
+else:
+    print("положительное нечетное число")
